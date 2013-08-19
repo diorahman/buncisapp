587612a22112bbb8172551a1dc5026ddcce36462
Dhi Aurrahman <diorahman@gmail.com>
2013-08-19 08:05:33 +0700

    FIX: glitch on push page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,31 +2,24 @@ angular.module('buncisApp.controllers', []).value('version', '0.1');
 
 var points = [
   {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'},
-  {title : 'Pantai Kuta', description: 'This is pantai kuta', author: 'Joni Iskandar', timestamp: 'Sat Aug 17 2013 11:29:18'}
-
 ]
 
-function MainController($scope, navigation, actionbars){
+function MainController($scope, $http, navigation, actionbars){
   navigation.clear()
 
   $scope.points = points
   $scope.push = function (path) {
     navigation.push(path);
   };
+
+  $scope.refresh = function(){
+
+    $http.post('http://dev.rockybars.com:3000/hello', { 'foo' : 'bar'}).success(function(data, status, headers, config){
+      console.log(status)
+      points.push(data)
+    })
+
+  }
 }
 
 function AddPointController($scope, navigation, actionbars){
